9dea4512c8acd70290047980d31d16411812a601
rohitsharma8433 <122019511+rohitsharma8433@users.noreply.github.com>
2023-03-18 17:03:59 +0530

    upload file

---
+++ greatest.py
@@ -0,0 +1,11 @@
+num1=int(input("enter a number "))
+num2=int(input("enter a number "))
+num3=int(input("enter a number "))
+if(num1>num2 and num1>num3):
+    print("number One is Greater",num1)
+elif(num2>num1 and num2>num3):
+    print("number two is Greater",num2)
+else:
+    print("number Three is Greater",num3)    
+
+         
---
+++ natural.py
@@ -0,0 +1,7 @@
+start=int(input("enter start number"))
+end=int(input("enter start number"))
+sum=0
+while(start<=end):
+    sum=sum+start
+    start+=1
+print(sum)
